38a087730290cd853137947b0a235e0590a89552
ngotanhoai <19520086@gm.uit.edu.vn>
2021-06-06 16:46:23 +0700

    UI

---
+++ WpfApp1/WpfApp1/PhieuNhapSachPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for PhieuNhapSachPage.xaml
+    /// </summary>
+    public partial class PhieuNhapSachPage : Page
+    {
+        public PhieuNhapSachPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Chip_Click(object sender, RoutedEventArgs e)
+        {
+            NhapSachWindow nhapSachWindow = new NhapSachWindow();
+            nhapSachWindow.ShowDialog();
+        }
+    }
+}
