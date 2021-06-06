2c168375e4caca0fa2281132a5c709b64ae7db7f
AnhTu <danganhtu2001@gmail.com>
2021-06-06 15:50:13 +0700

    Create database, DataProvider and Use MaterialDesign

--- BookStore/BookStore/Properties/Settings.Designer.cs
+++ BookStore/BookStore/Properties/Settings.Designer.cs
@@ -8,28 +8,21 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace BookStore.Properties {
-    
-    
+
+namespace BookStore.Properties
+{
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
-        
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
+    {
+
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-        
-        public static Settings Default {
-            get {
+
+        public static Settings Default
+        {
+            get
+            {
                 return defaultInstance;
-            }
-        }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=LAPTOP-R6R9E215;Initial Catalog=BOOKSTORE;Integrated Security=True")]
-        public string BOOKSTOREConnectionString {
-            get {
-                return ((string)(this["BOOKSTOREConnectionString"]));
             }
         }
     }
